37b2f6269487755d570e74ecd66ec110ec8b1928
Burak T <burak.taban.22@gmail.com>
2019-04-05 13:29:52 -0400

    Patient Code activity created

---
+++ app/src/main/java/com/brocktaban/patienttracking/PatientCode.kt
@@ -0,0 +1,12 @@
+package com.brocktaban.patienttracking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PatientCode : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_code)
+    }
+}
