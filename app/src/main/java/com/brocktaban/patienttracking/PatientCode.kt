fa1a6ebe61a3c34f2beb54bb18dae228808e3ca5
Burak T <burak.taban.22@gmail.com>
2019-04-05 14:01:15 -0400

    Designed Patiend Code Activity

--- app/src/main/java/com/brocktaban/patienttracking/PatientCode.kt
+++ app/src/main/java/com/brocktaban/patienttracking/PatientCode.kt
@@ -2,11 +2,39 @@ package com.brocktaban.patienttracking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter
+import android.view.inputmethod.EditorInfo
+import kotlinx.android.synthetic.main.activity_patient_code.*
+import org.jetbrains.anko.design.snackbar
 
 class PatientCode : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_code)
+
+        et_patient_code.filters = arrayOf(InputFilter.AllCaps(), InputFilter.LengthFilter(5))
+
+        et_patient_code.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val code = et_patient_code.text.toString()
+
+                checkCode(code)
+
+                return@setOnEditorActionListener true
+            }
+
+            return@setOnEditorActionListener false
+        }
+
+        btn_track_patient.setOnClickListener {
+            val code = et_patient_code.text.toString()
+
+            checkCode(code)
+        }
+    }
+
+    private fun checkCode(code: String) {
+        main.snackbar(code)
     }
 }
